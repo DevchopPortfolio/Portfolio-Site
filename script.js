b2d2c22bc3a10185772611796d56da7dc075ae55
Robert <email@email.com>
2021-09-29 16:20:32 +0100

    misc

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 
 // POPULATE VARIABLES WITH HTML ELEMENTS
 
-let titleholder = document.querySelector('#titleholder')
-let summaryholder = document.querySelector('#summaryholder')
-let tabpageholder = document.querySelector('#tabpageholder')
-let projecttitleholder = document.querySelector('#projecttitleholder')
-let projecttitleholder2 = document.querySelector('#projecttitleholder2')
-let linkurlholder = document.querySelector('#linkurlholder');
-let githuburlholder = document.querySelector('#githuburlholder');
+let titleHolder = document.querySelector('#titleholder')
+let summaryHolder = document.querySelector('#summaryholder')
+let tabpageHolder = document.querySelector('#tabpageholder')
+let projectTitleHolder = document.querySelector('#projecttitleholder')
+let projectTitleHolder2 = document.querySelector('#projecttitleholder2')
+let linkUrlHolder = document.querySelector('#linkurlholder');
+let githubUrlHolder = document.querySelector('#githuburlholder');
 
 
 
 
 // ADD EVENTLISTENER TO EACH BUTTON
 
-document.querySelectorAll('#menubar li').forEach( thisbutton => {
+document.querySelectorAll('#menubar li').forEach( thisButton => {
 
-    thisbutton.addEventListener('click', (pingreport) => {
-        buttonactivate(pingreport.target)
+    thisButton.addEventListener('click', (pingReport) => {
+        buttonActivate(pingReport.target)
     })
 
 })
@@ -29,10 +29,10 @@ document.querySelectorAll('#menubar li').forEach( thisbutton => {
 
 // CONTENT OF EVENTLISTENER FOR BUTTONS
 
-function buttonactivate (thisbutton) {
-
-        // console.log('button clicked')
-        // if(thisbutton.nodeName == 'p') {
+function buttonActivate (thisButton) {
+        
+        // if they click the text on the button instead of the button, target the button
+        // if(thisbutton.nodeName == 'SPAN') {            
         //     thisbutton = thisbutton.parentNode
         // }
 
@@ -41,41 +41,41 @@ function buttonactivate (thisbutton) {
         
         window.scrollTo(0,document.body.scrollHeight);                      //browser scrolls down
 
-        document.querySelectorAll('#menubar li').forEach(othertab => {
-            othertab.classList.remove('active')                             //remove 'active' class from all tabs
+        document.querySelectorAll('#menubar li').forEach(otherTab => {
+            otherTab.classList.remove('active')                             //remove 'active' class from all tabs
         })
 
-        thisbutton.classList.add('active')                                  //add 'active' class to this button
+        thisButton.classList.add('active')                                  //add 'active' class to this button
 
         // link to the correct post in the content library        
-        let thispost = document.querySelector('.post' + thisbutton.getAttribute('data-target'))
+        let thisPost = document.querySelector('.post' + thisButton.getAttribute('data-target'))
             
-        console.log('.post' + thisbutton.getAttribute('data-target'))
-        console.log(thispost)
+        // console.log('.post' + thisbutton.getAttribute('data-target'))
+        // console.log(thispost)
         
         
         //create links to all the content in thispost
-        let titletomove = thispost.querySelector('.title')        
-        let summarytomove = thispost.querySelector('.summary')
-        let tabpagestomove = thispost.querySelector('.tabpages')
-        let projecttitle = titletomove.innerText        
-        let linkurl = thispost.getAttribute('data-linkurl')
-        let githuburl = thispost.getAttribute('data-githuburl')
+        let titleToMove = thisPost.querySelector('.title')        
+        let summaryToMove = thisPost.querySelector('.summary')
+        let tabPagesToMove = thisPost.querySelector('.tabpages')
+        let projectTitle = titleToMove.innerText        
+        let linkUrl = thisPost.getAttribute('data-linkurl')
+        let githubUrl = thisPost.getAttribute('data-githuburl')
 
         //populate the template with thispost        
-        titleholder.innerHTML = ''
-        titleholder.appendChild(titletomove.cloneNode(true))        
-        summaryholder.innerHTML = ''
-        summaryholder.appendChild(summarytomove.cloneNode(true))        
-        tabpageholder.innerHTML = ''
-        tabpageholder.appendChild(tabpagestomove.cloneNode(true))
-        projecttitleholder.innerText = projecttitle
-        projecttitleholder2.innerText = projecttitle
-        linkurlholder.setAttribute('href', linkurl)
-        githuburlholder.setAttribute('href', githuburl)
+        titleHolder.innerHTML = ''
+        titleHolder.appendChild(titleToMove.cloneNode(true))        
+        summaryHolder.innerHTML = ''
+        summaryHolder.appendChild(summaryToMove.cloneNode(true))        
+        tabpageHolder.innerHTML = ''
+        tabpageHolder.appendChild(tabPagesToMove.cloneNode(true))
+        projectTitleHolder.innerText = projectTitle
+        projectTitleHolder2.innerText = projectTitle
+        linkUrlHolder.setAttribute('href', linkUrl)
+        githubUrlHolder.setAttribute('href', githubUrl)
 
         //activate tab1
-        tabactivate(document.querySelector('[data-target="#tab1"]'))
+        tabActivate(document.querySelector('[data-target="#tab1"]'))
 
 }
 
@@ -88,9 +88,9 @@ function buttonactivate (thisbutton) {
 
 // ADD EVENTLISTENER TO EACH TAB
 
-document.querySelectorAll('#tabsframe li').forEach( thistab => {
-    thistab.addEventListener('click', (e) => {
-        tabactivate(e.target)
+document.querySelectorAll('#tabsframe li').forEach( thisTab => {
+    thisTab.addEventListener('click', (e) => {
+        tabActivate(e.target)
     } ) 
 })
 
@@ -105,7 +105,7 @@ document.querySelectorAll('#tabsframe li').forEach( thistab => {
 // CONTENT OF EVENTLISTENER FOR TABS
 
 
-function tabactivate (thistab) {
+function tabActivate (thisTab) {
 
 
     // remove 'active' class from all tabs and all tabpages
@@ -114,16 +114,16 @@ function tabactivate (thistab) {
         othertab.classList.remove('active')
     })
 
-    document.querySelectorAll('#tabpageholder div').forEach(thisdiv => {
+    document.querySelectorAll('#tabpageholder .tabpage').forEach(thisdiv => {
         thisdiv.classList.remove('active')
     })
 
 
     // add 'active' class to the active tab and the active tabpage
 
-    thistab.classList.add('active')    
-    let targetstring = thistab.getAttribute('data-target')        
-    document.querySelector('#tabpageholder ' + targetstring).classList.add('active')
+    thisTab.classList.add('active')    
+    let targetString = thisTab.getAttribute('data-target')        
+    document.querySelector('#tabpageholder ' + targetString).classList.add('active')
                
 
 }
