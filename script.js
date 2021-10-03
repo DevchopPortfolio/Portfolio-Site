3131f86a00148735523361d268388c89e0e85622
Robert <salvazan@hotmail.com>
2021-10-03 11:04:24 +0100

    remove spaces from code

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 
-
 // POPULATE VARIABLES WITH HTML ELEMENTS
 
-let titleHolder = document.querySelector('#titleholder')
-let summaryHolder = document.querySelector('#summaryholder')
-let tabpageHolder = document.querySelector('#tabpageholder')
-let projectTitleHolder = document.querySelector('#projecttitleholder')
-let projectTitleHolder2 = document.querySelector('#projecttitleholder2')
+let titleHolder = document.querySelector('#titleholder');
+let summaryHolder = document.querySelector('#summaryholder');
+let tabpageHolder = document.querySelector('#tabpageholder');
+let projectTitleHolder = document.querySelector('#projecttitleholder');
+let projectTitleHolder2 = document.querySelector('#projecttitleholder2');
 let linkUrlHolder = document.querySelector('#linkurlholder');
 let githubUrlHolder = document.querySelector('#githuburlholder');
-
-
 
 
 // ADD EVENTLISTENER TO EACH BUTTON
@@ -24,18 +21,10 @@ document.querySelectorAll('#menubar li').forEach( thisButton => {
         //     window.scrollTo(0, document.body.scrollHeight);                      //browser scrolls down
         // }       
         
-        buttonActivate(pingReport.target)
+        buttonActivate(pingReport.target);
 
-    })
-
-})
-
-
-
-
-
-
-
+    });
+});
 
 
 // CONTENT OF EVENTLISTENER FOR BUTTONS
@@ -47,102 +36,75 @@ function buttonActivate (thisButton) {
         //     thisbutton = thisbutton.parentNode
         // }
 
-        document.querySelector('#lowerframe').classList.remove('invisibility')
-        // document.querySelector('#placeholder').classList.add('invisibility')
-      
+        document.querySelector('#lowerframe').classList.remove('invisibility');
+        // document.querySelector('#placeholder').classList.add('invisibility')      
 
         document.querySelectorAll('#menubar li').forEach(otherTab => {
-            otherTab.classList.remove('active')                             //remove 'active' class from all tabs
+            otherTab.classList.remove('active');                             //remove 'active' class from all tabs
         })
 
-        thisButton.classList.add('active')                                  //add 'active' class to this button
+        thisButton.classList.add('active');                                  //add 'active' class to this button
 
         // link to the correct post in the content library        
-        let thisPost = document.querySelector('.post' + thisButton.getAttribute('data-target'))
+        let thisPost = document.querySelector('.post' + thisButton.getAttribute('data-target'));
             
         // console.log('.post' + thisbutton.getAttribute('data-target'))
-        // console.log(thispost)
-        
+        // console.log(thispost)        
         
         //create links to all the content in thispost
-        let titleToMove = thisPost.querySelector('.title')        
-        let summaryToMove = thisPost.querySelector('.summary')
-        let tabPagesToMove = thisPost.querySelector('.tabpages')
-        let projectTitle = titleToMove.innerText        
-        let linkUrl = thisPost.getAttribute('data-linkurl')
-        let githubUrl = thisPost.getAttribute('data-githuburl')
+        let titleToMove = thisPost.querySelector('.title');
+        let summaryToMove = thisPost.querySelector('.summary');
+        let tabPagesToMove = thisPost.querySelector('.tabpages');
+        let projectTitle = titleToMove.innerText;
+        let linkUrl = thisPost.getAttribute('data-linkurl');
+        let githubUrl = thisPost.getAttribute('data-githuburl');
 
         //populate the template with thispost        
-        titleHolder.innerHTML = ''
-        titleHolder.appendChild(titleToMove.cloneNode(true))        
-        summaryHolder.innerHTML = ''
-        summaryHolder.appendChild(summaryToMove.cloneNode(true))        
-        tabpageHolder.innerHTML = ''
-        tabpageHolder.appendChild(tabPagesToMove.cloneNode(true))
-        projectTitleHolder.innerText = projectTitle
-        projectTitleHolder2.innerText = projectTitle
-        linkUrlHolder.setAttribute('href', linkUrl)
-        githubUrlHolder.setAttribute('href', githubUrl)
+        titleHolder.innerHTML = '';
+        titleHolder.appendChild(titleToMove.cloneNode(true));
+        summaryHolder.innerHTML = '';
+        summaryHolder.appendChild(summaryToMove.cloneNode(true));
+        tabpageHolder.innerHTML = '';
+        tabpageHolder.appendChild(tabPagesToMove.cloneNode(true));
+        projectTitleHolder.innerText = projectTitle;
+        projectTitleHolder2.innerText = projectTitle;
+        linkUrlHolder.setAttribute('href', linkUrl);
+        githubUrlHolder.setAttribute('href', githubUrl);
 
         //activate tab1
-        tabActivate(document.querySelector('[data-target="#tab1"]'))
-
-}
-
-
-
-
-
-
+        tabActivate(document.querySelector('[data-target="#tab1"]'));
+};
 
 
 // ADD EVENTLISTENER TO EACH TAB
-
 document.querySelectorAll('#tabsframe li').forEach( thisTab => {
     thisTab.addEventListener('click', (e) => {
-        tabActivate(e.target)
-    } ) 
-})
-
-
-
-
-
-
-
+        tabActivate(e.target);
+    });
+});
 
 
 // CONTENT OF EVENTLISTENER FOR TABS
 
-
 function tabActivate (thisTab) {
-
 
     // remove 'active' class from all tabs and all tabpages
     
     document.querySelectorAll('#tabsframe li').forEach(othertab => {
-        othertab.classList.remove('active')
-    })
+        othertab.classList.remove('active');
+    });
 
     document.querySelectorAll('#tabpageholder .tabpage').forEach(thisdiv => {
-        thisdiv.classList.remove('active')
-    })
-
+        thisdiv.classList.remove('active');
+    });
 
     // add 'active' class to the active tab and the active tabpage
-
-    thisTab.classList.add('active')    
-    let targetString = thisTab.getAttribute('data-target')        
-    document.querySelector('#tabpageholder ' + targetString).classList.add('active')
-               
-
-}
-
-
-
-
-
+    thisTab.classList.add('active');
+    let targetString = thisTab.getAttribute('data-target');       
+    document.querySelector('#tabpageholder ' + targetString).classList.add('active');
+      
+};
 
 
 // make first button active upon first load
-buttonActivate(document.querySelector('#menubar :first-child'))
+buttonActivate(document.querySelector('#menubar :first-child'));
